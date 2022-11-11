2028c4c5c8a6b393e50265ed52713613ac0f6e2c
Luis <luissolivaresp@gmail.com>
2022-11-10 23:53:11 -0500

    origin update

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://prueba-production-be77.up.railway.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
